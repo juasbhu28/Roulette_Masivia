11c74c3f34196f40e04c95844f60dc56cc36686f
JuanSe <juasbhu@hotmail.com>
2020-12-07 09:11:56 -0500

    Docker and readme

--- RouletteMasivia/RouletteMasivia/Startup.cs
+++ RouletteMasivia/RouletteMasivia/Startup.cs
@@ -62,7 +62,7 @@ namespace RouletteMasivia
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=swagger}/{action=Index}");
             });
         }
     }
